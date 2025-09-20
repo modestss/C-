dc67935beffc8f420239a799de07b826159211cd
马庭宝 <140888790+modestss@users.noreply.github.com>
2025-09-20 22:25:09 +0800

    Update 空间中点到面的距离.c

--- 空间中点到面的距离.c
+++ 空间中点到面的距离.c
@@ -5,6 +5,6 @@ int main(){
   scanf("%f %f %f",&x,&y,&z);
   scanf("%f %f %f %f",&A,&B,&C,&D);
   d = fabs(A*x+B*y+C*z+D)/sqrt(A*A+B*B+C*C);//fabs()用于取绝对值，sqrt()用于开方求平方根
-  printf("%.3f",d);
+  printf("%.3f",d);//%.3f作用是结果保留小数点后三位
   return 0;
 }
